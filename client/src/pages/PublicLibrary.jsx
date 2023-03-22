c6abcde21edb92b8e8558699ddae68c6fc850cc0
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2023-03-23 02:45:25 +0530

    added public library

--- client/src/pages/PublicLibrary.jsx
+++ client/src/pages/PublicLibrary.jsx
@@ -3,13 +3,11 @@ import '../style/shelf.css'
 
 function PublicLibrary() {
   return (
-    <div className="full library
-    ">
-      <div className="shelf_bg shelf">
-        <div
-          className="container"
-          ontouchstart="this.classList.toggle('hover')"
-        >
+    <div className="main">
+      <div className="library">
+      <div className="shelf">
+        <div className=" container">
+
           <div className="card">
             <div className="card_front" />
             <div className="card_back">
@@ -18,16 +16,16 @@ function PublicLibrary() {
                   When mysterious letters start arriving on his doorstep, Harry
                   Potter has never heard of Hogwarts School of Witchcraft and
                   Wizardry. They are swiftly confiscated by his aunt and uncle.
-                  Then, on Harry’s eleventh birthday, a strange man bursts in
+                  Then, on Harry's eleventh birthday, a strange man bursts in
                   with some important news: Harry Potter is a wizard and has
                   been awarded a place to study at Hogwarts. And so the first of
                   the Harry Potter adventures is set to begin.
                 </p>
               </div>
             </div>
-          </div>
         </div>
-        {/* <div className="container" ontouchstart="this.classList.toggle('hover');">
+        </div>
+        <div className="container" ontouchstart="this.classList.toggle('hover');">
         <div className="card">
           <div className="card_front" />
           <div className="card_back">
@@ -64,6 +62,8 @@ function PublicLibrary() {
         </div>
       </div>
     </div>
+
+    
     <div className="shelf">
       <div className="container" ontouchstart="this.classList.toggle('hover');">
         <div className="card">
@@ -118,9 +118,10 @@ function PublicLibrary() {
             </div>
           </div>
         </div>
-      </div> */}
       </div>
-    </div>
+</div>
+      </div>
+      </div>
   );
 }
 
