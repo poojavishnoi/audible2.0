8d928bb62f69d6bf468be3d74dfa1ccd0cf8fb01
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2023-04-27 21:40:05 +0530

    flip logic changed

--- client/src/components/NewFlipBook.js
+++ client/src/components/NewFlipBook.js
@@ -23,16 +23,13 @@ console.log(audioTime, "audioTime");
       // const subtitles = subtitleSrc.trim()
       //   .split(/\n\s*\n/)
 
-      // fetch(subtitleSrc)
-      //   .then((response) => response.text())
-      //   .then((data) => {
-      //     // Parse the SRT file into an array of subtitle objects
-      //     const subtitles = data
-      //       .trim()
-      //       .split("\n\n")
-
-     const subtitles = subtitleSrc.trim()
-        .split(/\n\s*\n/)
+      fetch(subtitleSrc)
+        .then((response) => response.text())
+        .then((data) => {
+          // Parse the SRT file into an array of subtitle objects
+          const subtitles = data
+            .trim()
+            .split("\n\n")
             .map((subtitle) => {
               const [index, time, text] = subtitle?.split("\n");
               return {
@@ -44,7 +41,7 @@ console.log(audioTime, "audioTime");
             });
 
           setSubtitles(subtitles);
-   
+          });
     }
 
   },[]);
@@ -59,26 +56,42 @@ useEffect(() => {
   subtitles? pageDistribution() : <>  </>
 }, [subtitles, audioSrc]);
 
-  useEffect(() => {
 
-    for (let index = 0; index < pages.length; index++) {
+console.log(pages, "pages");
 
-     if (time > pages[index].content.slice(-1)[0].end) {
-        audioRef.current.gotoNextPage();
-      }
-      if (time < pages[index].content[0].start) {
-        audioRef.current.gotoPreviousPage();
-      }
+useEffect(() => {
+  // Initialize currentPageIndex to 0
+  let currentPageIndex = 1;
+
+  // Loop through each page in the array
+  for (let i = 0; i < pages.length; i++) {
+    const pageStartTime = pages[i]?.content[0].start; // Get the start time of the first element in the page's content array
+
+    // If the current time is greater than or equal to the end time of the current page, update currentPageIndex to the next page index
+    if (time >= pages[i]?.content.slice(-1)[0].end) {
+      currentPageIndex = i + 1;
     }
-  }, [time, pages]);
+    // If the current time is less than the start time of the current page and the current page is not the first page, update currentPageIndex to the previous page index
+     if (i > 0 && time < pageStartTime) {
+      currentPageIndex = i - 1;
+      break;
+    }
+  }
+  console.log(  currentPageIndex, "currentPageIndex");
+
+  setTimeout(() => {
+    audioRef.current.gotoPage(currentPageIndex);
+
+  }, 100);
+    
+}, [time, pages, audioRef]);
+
 
 
   const pageDistribution = () => {
     var pageArray = [];
     var subtitleIndex = 0;
     var pageCount = 1;
-
-
 
     while (subtitles.length > subtitleIndex) {
       var pageFilled = 0;
@@ -159,8 +172,8 @@ useEffect(() => {
         current
         onListen={handleAudioTimeUpdate}
         //  src= {`data:audio/wav;base64,${audioSrc}`}
-        //src={audioSrc.src}
-        src={duration}
+        src={audioSrc}
+        // src={duration}
         type="audio/wav"
         //onLoadedMetaData={handleLoadMetadata}
         className="mb-4 "
@@ -169,6 +182,7 @@ useEffect(() => {
         autoPlay = {false}
       />
 
+    
 
     <div >
       <FlipPage
@@ -178,7 +192,7 @@ useEffect(() => {
         orientation="horizontal"
         // responsive={true}
         className="flip"
-        width={1100}
+        width={1500}
         style={{
           padding: "0",
           margin: "0",
@@ -192,7 +206,7 @@ useEffect(() => {
         {pages.map((page, pindex) => (
           <article
             key={pindex}
-            className="p-7 relative text-center flex flex-col text-black"
+            className="p-7 relative text-center  flex flex-col text-black"
           >
 
             {page.content.map((subtitle, index) => {
@@ -201,11 +215,13 @@ useEffect(() => {
                 <div className=  " md:w-[45%] text-left">
                   <p
                     key={index}
-                    className={`p-2 ${
+                    className={`p-2 fade-in-out ${
                       subtitle.text === currentSubtitle?.text
                         ? "font-bold text-red-800 text-3xl"
                         : "font-normal text-xl"
                     }`}
+
+                    
                   >
                     {subtitle?.text}
                   </p>
